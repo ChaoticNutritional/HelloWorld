aa6dda1a88d95788a8ed9cbbc5645637bf26f4ac
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2022-04-19 18:38:12 -0400

    Initial commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class HelloWorld : MonoBehaviour
+{
+    private string hello = "Hello World!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("starting " + hello);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(hello);
+    }
+}
